fa9714d4aaee03b40c2d13b97214d8f4b9f18e08
youssefahmed5491 <youssef.gy87@gmail.com>
2022-03-06 21:32:47 +0200

    initial commit

---
+++ resources/js/components/Habal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Habal = () => {
+    return <div>blabla</div>;
+};
+
+export default Habal;
---
+++ resources/js/components/Index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index = () => {
+    return <div>ffffffffffff</div>;
+};
+
+export default Index;
