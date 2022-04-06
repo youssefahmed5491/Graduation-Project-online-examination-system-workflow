581cdaef28c0ba56aa3dfc8e295e5d14dfd9de39
Tonyfayez <tonyfayez111@gmail.com>
2022-04-06 13:25:49 +0200

    AllUsersHome.js b2t fl main bs <3

--- app/Models/professor.php
+++ app/Models/professor.php
@@ -8,11 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class professor extends Model
 {
     protected $connection="mysql2";
+<<<<<<< HEAD
     // protected $fillable =[
     //     "username",
     //     "email",
     //     "passowrd"
     // ];
+=======
+   
+>>>>>>> 5723684 (AllUsersHome.js b2t fl main bs <3)
     use HasFactory;
  
 }
